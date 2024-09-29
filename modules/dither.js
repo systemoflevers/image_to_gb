fa2d385cd90cd3a28f45dcc2d2bd3813e1e26c02
Jesse Doherty <jewido@gmail.com>
2024-09-29 16:52:55 -0400

    Fixes to dither algorithm.a

--- modules/dither.js
+++ modules/dither.js
@@ -1,4 +1,15 @@
+/**
+ * @fileoverview An implementation of Floyd–Steinberg dithering based on
+ * luminance to convert images to the 4 GameBoy green colours.
+ */
 
+/**
+ * Compute the luminance of an RGB value.
+ * @param {number} r 
+ * @param {number} g 
+ * @param {number} b 
+ * @returns {number}
+ */
 function computeLuminance(r, g, b) {
   // based on https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
   return 0.2126 * r + 0.7152 * g + 0.0722 * b;
@@ -39,21 +50,21 @@ function propagateError(errors, x, y, imageData) {
       imageData.data[(i + 1) * 4 + c] += (errors[c] * 7) / 16;
     if (y === imageData.height - 1) continue;
     if (x !== 0)
-      imageData.data[(i + 1) * 4 + c] += (errors[c] * 3) / 16;
-    imageData.data[(i + 1) * 4 + c] += (errors[c] * 5) / 16;
-    imageData.data[(i + 1) * 4 + c] += (errors[c] * 1) / 16;
+      imageData.data[(i + imageData.width - 1) * 4 + c] += (errors[c] * 3) / 16;
+    imageData.data[(i + imageData.width) * 4 + c] += (errors[c] * 5) / 16;
+    imageData.data[(i + imageData.width + 1) * 4 + c] += (errors[c] * 1) / 16;
   }
   return imageData;
 }
 
 /**
- * 
+ * Modifies `imageData` by applying Floyd–Steinberg dithering based on luminance.
  * @param {ImageData} imageData 
  * @param {=number} howMuch A value between 0 and 100 to indicate how much the
  *  dither should be applied. 0 is not at all, 100 is normal full dither.
  *  Intended to be used to fade between non-dithered and dithered.
- * @param {=targetColours}
- * @returns {ImageData}
+ * @param {=number[][]} targetColours
+ * @returns {ImageData} The modified `imageData`.
  */
 function ditherToColour(imageData, howMuch = 100, targetColours = kGreenColours) {
   const targetLuminances = targetColours.map(([r, g, b]) => computeLuminance(r, g, b));
@@ -75,6 +86,12 @@ function ditherToColour(imageData, howMuch = 100, targetColours = kGreenColours)
   return imageData;
 }
 
+/**
+ * Modifies `imageData` by appling Floyd–Steinberg dithering based on luminance.
+ * @param {ImageData} imageData 
+ * @param {=number[][]} targetColours
+ * @returns An array of pixels where each pixel is an intex into `targetColours`.
+ */
 function ditherToColourIndex(imageData, targetColours = kGreenColours) {
   const targetLuminances = targetColours.map(([r, g, b]) => computeLuminance(r, g, b));
   const colourIndexes = new Uint8Array(imageData.width * imageData.height);
